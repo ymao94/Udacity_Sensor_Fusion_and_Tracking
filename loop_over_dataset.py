corrected the calculation of detection boxes

loop_over_dataset.py
@@ -72,14 +72,14 @@ configs_det.use_labels_as_objects = False # True = use groundtruth labels as obj
 
 ## Initialize tracking
 KF = Filter() # set up Kalman filter 
-association = Association() # init data association
+association = Association() # init data association 
 manager = Trackmanagement() # init track manager
 lidar = None # init lidar sensor object
 camera = None # init camera sensor object
 np.random.seed(10) # make random values predictable
 
 ## Selective execution and visualization
-exec_data = ['pcl_from_rangeimage']
+exec_data = []
 exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'] #'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'  options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
 exec_tracking = [] # options are 'perform_tracking'
 exec_visualization = ['show_detection_performance'] # options are , 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'